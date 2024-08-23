4df1d719593281e503ec4f8967d332b63233ba74
slm1337 <e1337ajke@gmail.com>
2024-08-23 15:48:15 +0300

    Added move generation & find reachable positions in base class

--- app/src/main/java/com/example/schach1337/logic/pieces/Piece.kt
+++ app/src/main/java/com/example/schach1337/logic/pieces/Piece.kt
@@ -1,7 +1,11 @@
 package com.example.schach1337.logic.pieces
 
+import com.example.schach1337.logic.Board
+import com.example.schach1337.logic.Direction
 import com.example.schach1337.logic.PieceType
 import com.example.schach1337.logic.Player
+import com.example.schach1337.logic.Position
+import com.example.schach1337.logic.moves.Move
 
 
 abstract class Piece {
@@ -10,4 +14,30 @@ abstract class Piece {
     var hasMoved : Boolean = false
 
     abstract fun copy() : Piece
+
+    abstract fun getMoves(from : Position, board : Board) : Sequence<Move>
+
+    protected fun movePositionsInDir(from: Position, board: Board, dir: Direction): Sequence<Position> = sequence {
+        var pos = from + dir
+        while (Board.isInside(pos)) {
+            if (board.isEmpty(pos)) {
+                yield(pos)
+                pos += dir
+                continue
+            }
+
+            val piece = board[pos]
+
+            if (piece != null && piece.color != color) {
+                yield(pos)
+            }
+
+            break
+        }
+    }
+
+    protected fun movePositionInDirs(from: Position, board: Board, dirs: Array<Direction>): Sequence<Position> {
+        return dirs.asSequence().flatMap { dir -> movePositionsInDir(from, board, dir) }
+    }
+
 }

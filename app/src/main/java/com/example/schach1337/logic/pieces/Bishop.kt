aa13049cf8c1f947a191fb8b90636e078441f648
slm1337 <e1337ajke@gmail.com>
2024-08-23 20:09:09 +0300

    Added implementation for moving Bishop, Queen, Rook

--- app/src/main/java/com/example/schach1337/logic/pieces/Bishop.kt
+++ app/src/main/java/com/example/schach1337/logic/pieces/Bishop.kt
@@ -1,11 +1,25 @@
 package com.example.schach1337.logic.pieces
 
+import com.example.schach1337.logic.Board
+import com.example.schach1337.logic.Direction
 import com.example.schach1337.logic.PieceType
 import com.example.schach1337.logic.Player
+import com.example.schach1337.logic.Position
+import com.example.schach1337.logic.moves.Move
+import com.example.schach1337.logic.moves.NormalMove
 
 class Bishop : Piece {
     override var type: PieceType = PieceType.Bishop
     override var color: Player = Player.None
+
+    companion object{
+        private val dirs : Array<Direction> = arrayOf(
+            Direction.NorthWest,
+            Direction.NorthEast,
+            Direction.SouthWest,
+            Direction.SouthEast
+        )
+    }
 
     constructor(color : Player){
         this.color = color
@@ -15,5 +29,9 @@ class Bishop : Piece {
         val copy = Bishop(color)
         copy.hasMoved = hasMoved
         return copy
+    }
+
+    override fun getMoves(from: Position, board: Board): Sequence<Move> {
+        return movePositionInDirs(from, board, dirs).map { to -> NormalMove(from, to) }
     }
 }
--- app/src/main/java/com/example/schach1337/logic/pieces/Rook.kt
+++ app/src/main/java/com/example/schach1337/logic/pieces/Rook.kt
@@ -1,11 +1,25 @@
 package com.example.schach1337.logic.pieces
 
+import com.example.schach1337.logic.Board
+import com.example.schach1337.logic.Direction
 import com.example.schach1337.logic.PieceType
 import com.example.schach1337.logic.Player
+import com.example.schach1337.logic.Position
+import com.example.schach1337.logic.moves.Move
+import com.example.schach1337.logic.moves.NormalMove
 
 class Rook : Piece {
     override var type: PieceType = PieceType.Rook
     override var color: Player = Player.None
+
+    companion object{
+        private val dirs : Array<Direction> = arrayOf(
+            Direction.North,
+            Direction.South,
+            Direction.East,
+            Direction.West
+        )
+    }
 
     constructor(color : Player){
         this.color = color
@@ -16,4 +30,9 @@ class Rook : Piece {
         copy.hasMoved = hasMoved
         return copy
     }
+
+    override fun getMoves(from: Position, board: Board): Sequence<Move> {
+        return movePositionInDirs(from, board, dirs).map { to -> NormalMove(from, to) }
+    }
+
 }
